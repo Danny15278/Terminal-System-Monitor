205457372f88c7a9e9482a4d460d5baa3eb58082
Danny15278 <danny26574@gmail.com>
2025-05-29 23:42:51 +1200

    Adding functionality for reading memory

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ fn main() {
     loop {
         sys.refresh_cpu_usage(); // Refresh CPU data
 
-        let cpu_usage = sys.global_cpu_usage(); // Get its usage percentage
+        let cpu_usage = sys.global_cpu_usage(); // CPU usage percentage
+
+        let total_memory = sys.total_memory() as f64 / 1_048_576.0;
+        let used_memory = sys.used_memory() as f64 / 1_048_576.0;
+
+        let uptime_secs = System::uptime();
+    let hours = uptime_secs / 3600;
+    let minutes = (uptime_secs % 3600) / 60;
+    let seconds = uptime_secs % 60;
 
         println!("CPU Usage: {:.2}%", cpu_usage);
 
-        // Sleep to allow enough time for updated data
         thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
     }
 }
